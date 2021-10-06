ec52748d46b604712366668dd92c47cfa74c2ed4
dina1233 <92003526+dina1233@users.noreply.github.com>
2021-10-05 21:14:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="apple-touch-icon" type="image/png" href="img/icon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+    <link rel="mask-icon" type="" href="img/logo.svg" color="#111" />
+    <title>Ejercicio PHP 02</title>
+    <link rel="stylesheet" href="css/reset.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+  </head>
+  <body translate="no">
+    <div class="background">
+      <div class="container">
+        <div class="panel pricing-table">
+          <div class="pricing-plan">
+            <h2 class="pricing-header">Ejercicio Ciclos</h2>
+            <ul class="pricing-features">
+              <li class="pricing-features-item">
+              <?php 
+                //Digite el código aquí
+              ?>
+              </li>
+            </ul>
+            <span class="pricing-price">Fin</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
